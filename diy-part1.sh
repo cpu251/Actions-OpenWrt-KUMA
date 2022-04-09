e1ff2dc6007c9fdbd31a0ab1d16984c881377d7d
KUMA <cpu251@users.noreply.github.com>
2022-04-09 11:56:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@ git config --global http.sslverify false
 git config --global https.sslverify false
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
